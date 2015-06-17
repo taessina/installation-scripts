2165a44fdd81fc5c9f0414970267878cfd2baa74
Yao Bin <alvinthen890703@gmail.com>
2015-06-17 12:32:19 +0800

    Update rails-install-ubuntu.sh

--- rails-install-ubuntu.sh
+++ rails-install-ubuntu.sh
@@ -13,12 +13,12 @@ sudo apt-get install imagemagick --fix-missing -y
 
 echo "Installs RVM (Ruby Version Manager) for handling Ruby installation"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s -- --version 1.26.11
+curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installs Ruby"
-rvm install 2.2.1
-rvm use 2.2.1 --default
+rvm install 2.2.2
+rvm use 2.2.2 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
